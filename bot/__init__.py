5c8737344563797d7c5be3881f756dcc67aee3df
olkhovich <olkhovichs@icloud.com>
2020-12-11 01:44:35 +0300

    added protecting token

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,7 +1,7 @@
 import os
 
 def hideToken():
-    with open('../token.txt') as file:
+    with open('token.txt') as file:
         for line in file:
             token = line
     return token
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 from bot import hideToken
+
+import telebot
+bot = telebot.TeleBot(hideToken())
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, 'test')
+
+bot.polling(none_stop=True, interval=0)
