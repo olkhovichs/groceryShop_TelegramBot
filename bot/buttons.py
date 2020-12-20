a723c27443184f72246a9b88d59f7d22c31e71ff
olkhovich <olkhovichs@icloud.com>
2020-12-20 18:45:07 +0300

    buttons: changing keyboard to inline

--- bot/buttons.py
+++ bot/buttons.py
@@ -2,11 +2,9 @@ from aiogram.types import ReplyKeyboardRemove, \
         ReplyKeyboardMarkup, KeyboardButton, \
         InlineKeyboardMarkup, InlineKeyboardButton
 
-btn_categ = KeyboardButton('Категории') # 1
-btn_cart = KeyboardButton('Корзина') # 2
-btn_about = KeyboardButton('Информация') # 3
-btn_exit = KeyboardButton('Выход') # 4
-markup_menu = ReplyKeyboardMarkup()
-markup_menu.add(btn_categ).add(btn_cart).add(btn_about).add(btn_exit)
-#markup_menu.row(btn_categ, btn_cart)
-#markup_menu.row(btn_about, btn_exit)
+btn_categ = InlineKeyboardButton('Категории', callback_data='categ') # 1
+btn_cart = InlineKeyboardButton('Корзина', callback_data='cart') # 2
+btn_about = InlineKeyboardButton('Информация', callback_data='about') # 3
+btn_exit = InlineKeyboardButton('Выход', callback_data='exit') # 4
+markup_menu = InlineKeyboardMarkup(row_width = 2)
+markup_menu.add(btn_categ, btn_cart, btn_about, btn_exit)
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from bot.config import TOKEN
 import bot.buttons as btn
 
-
 from aiogram import Bot, types
 from aiogram.utils import executor
 from aiogram.utils.markdown import text
@@ -12,7 +11,16 @@ disp = Dispatcher(bot)
 
 @disp.message_handler(commands = ['start'])
 async def init_menu(message: types.Message):
-    await message.reply("Привет!", reply_markup = btn.markup_menu)
+    await message.reply("Выберите действие", reply_markup = btn.markup_menu)
 
+
+'''
+@disp.callback_query_handler(func=lambda  call: True)
+def event_buttons(call):
+    if call.data = 'categ':
+'''
+
+
+##
 if __name__ == '__main__':
     executor.start_polling(disp)
