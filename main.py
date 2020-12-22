3cd2923d85150716597d920cd06b5d4356b1e800
olkhovich <olkhovichs@icloud.com>
2020-12-22 22:27:39 +0300

    main: add exit func

--- main.py
+++ main.py
@@ -11,16 +11,19 @@ dp = Dispatcher(bot)
 
 # main
 @dp.message_handler(commands = ['start'])
-async def menu(message: types.Message):
+async def main_menu(message: types.Message):
     await message.reply("Выберите действие:", reply_markup = btn.init_menu())
-
+'''
 # categories
 @dp.callback_query_handler(lambda call: call.data == 'categ')
-async def product_menu1(callback_query: types.CallbackQuery):
+async def categ_menu(callback_query: types.CallbackQuery):
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(callback_query.from_user.id, 'Категории:', reply_markup=btn.categ_menu())
-
-
+'''
+# exit
+dp.callback_query_handler(lambda call: call.data == 'exit')
+async def exit_bot(callback_query: types.CallbackQuery):
+    await bot.delete_message(message.chat_id)
 
 
 
