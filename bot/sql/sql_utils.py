c0eacc2bd619891c1c5e8e88893eb9c32d4fa7dc
olkhovich <olkhovichs@icloud.com>
2021-01-14 23:46:09 +0300

    sql: added outputting specific products

--- bot/sql/sql_utils.py
+++ bot/sql/sql_utils.py
@@ -6,7 +6,10 @@ def show_categories():
     try:
         conn = sqlite3.connect('/Users/semenolhovic/Python_Projects/groceryShop/bot/sql/bot.db')
         cursor = conn.cursor()
-        query = """SELECT * FROM 'categories'"""
+        query = """
+        SELECT * 
+        FROM 'categories'
+        """
         cursor.execute(query)
         categories = cursor.fetchall()
     except Error:
@@ -15,3 +18,21 @@ def show_categories():
         conn.close()
 
     return categories
+ # заполнить таблицу автоматически!!
+def print_products(product_id):
+    try:
+        conn = sqlite3.connection('/Users/semenolhovic/Python_Projects/groceryShop/bot/sql/bot.db')
+        cursor = conn.cursor()
+        query = """
+        SELECT * 
+        FROM 'products'
+        WHERE 'parent_id' =:product_id
+        """
+        cursor.execute(query, {'product_id': product_id})
+        products = cursor.fetchall()
+    except Error:
+        print(Error)
+    finally:
+        conn.close()
+
+    return products
